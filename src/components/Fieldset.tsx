385696671eaa3ef660b68aa6f2ecbf09677ebfcb
MuhibAnwar <muhibanwar065@gmail.com>
2024-11-22 15:04:22 +0500

    changes in global.css,footer,feildset,and mainly in css

--- src/components/Fieldset.tsx
+++ src/components/Fieldset.tsx
@@ -9,22 +9,22 @@ const Fieldset = () => {
       <h2 id='line'>What are you looking for?</h2>
       <fieldset>
         <Link href="/result">
-          <button  style={{marginLeft:'300px'}}>SSC II Result 2024</button>
+          <button  style={{marginLeft:'100px'}}>SSC II Result 2024</button>
         </Link>
       </fieldset>
       <fieldset>
         <Link href="/policy">
-          <button style={{marginLeft:'300px'}}>School policy</button>
+          <button style={{marginLeft:'100px'}}>School policy</button>
         </Link>
       </fieldset>
       <fieldset>
         <Link href="/addmission">
-          <button style={{marginLeft:'300px'}}>Addmission form</button>
+          <button style={{marginLeft:'100px'}}>Addmission form</button>
         </Link>
       </fieldset>
       <fieldset>
         <Link href="/about">
-          <button style={{marginLeft:'300px'}}>About us</button>
+          <button style={{marginLeft:'100px'}}>About us</button>
         </Link>
       </fieldset>
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React
 
  const Footer= () => {
     return (
-      <div id='footer'style={{marginTop:'100px',alignContent:'center',display:'flex',justifyContent:'center'}}>
+      <div id='footer'style={{marginTop:'100px',alignContent:'center',display:'flex',justifyContent:'center',backgroundColor:'#e2d9e7'}}>
     <p>© 2024 Tauheed Academy. All rights reserved</p>
         
         
